cade6e7ac007f3125c9e1a5fee0e9133afb374d6
RabeloJohn <contactjohnrabelo@gmail.com>
2022-08-22 21:03:57 -0300

    changes4

--- src/pages/OptAbout.js
+++ src/pages/OptAbout.js
@@ -15,11 +15,11 @@ function AboutUs() {
       <br />
       <p className={classes.header}>
         <br />
-        Dos Sombreros opened its doors in the spring of 2012 in the heart of the
-        bustling south lanes of Brighton. In the autumn of 2013 we opened our
-        ‘Day of the Dead’ themed tequila and cocktail bar above the restaurant.
-        Containing original pieces of artwork from Jony Daga (check out his work
-        in our gallery section). <br /> <br />
+        Dos Sombreros restaurant opened its doors in the spring of 2012 in the
+        heart of the bustling south lanes of Brighton. In the autumn of 2013 we
+        opened our ‘Day of the Dead’ themed tequila and cocktail bar above the
+        restaurant. Containing original pieces of artwork from Jony Daga (check
+        out his work in our gallery section). <br /> <br />
         Dos Sombreros tequila and cocktail bar opened on the third floor of our
         Mexican restaurant in November 2013. A perfect place to have drinks if
         you are waiting for friends to arrive, or a private party. We also offer
